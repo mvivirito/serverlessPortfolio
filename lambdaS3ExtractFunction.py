b516698f95fa912974d7b0942b8626cc9f9bd9a7
Michael Vivirito <mvivirito@gmail.com>
2019-12-24 14:39:10 -0800

    added try statement to lamnda func

--- lambdaS3ExtractFunction.py
+++ lambdaS3ExtractFunction.py
@@ -9,39 +9,52 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:983784055278:DeployGloriaPortfolioTopic')
 
+
+    #for manual use outside of code pipeline
     location = {
-        "bucketName": 'build.www.gloriashulman.info',
-        "objectKey": 'gloriabuildbucket.zip'
+        "bucketName": 'gloriashulman-portfoliobuild-bucket',
+        "objectKey": 'portfoliobuild.zip'
     }
 
-    job = event.get("CodePipeline.job")
 
-    if job:
-        for artifact in job["data"]["inputArtifacts"]:
-            if artifact["name"] == "MyAppBuild":
-                location = artifact["location"]["s3Location"]
+    try:
 
-    gloriashulman_bucket = s3.Bucket('www.gloriashulman.info')
-
-    build_bucket = s3.Bucket(location["bucketName"])
-
-    portfolio_zip = BytesIO()
+        job = event.get("CodePipeline.job")
 
 
-    build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
+        #make sure the artifact name matched the output of codebuild name!
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "BuildArtifact": #name from codepipeline
+                    location = artifact["location"]["s3Location"]
+
+        print("Building portfolio from " + str(location))
+
+        gloriashulman_bucket = s3.Bucket('www.gloriashulman.info')
+
+        build_bucket = s3.Bucket(location["bucketName"])
+
+        portfolio_zip = BytesIO()
 
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        listOfFileNames = myzip.namelist()
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
-        for fileName in listOfFileNames:
-            obj = myzip.open(fileName)
-            gloriashulman_bucket.upload_fileobj(obj, fileName, ExtraArgs={'ContentType': mimetypes.guess_type(fileName) [0]})
-            gloriashulman_bucket.Object(fileName).Acl().put(ACL='public-read')
 
-    topic.publish(Message="Portfolio Deplyed!")
-    if job:
-        codepipeline = boto3.client('codepipeline')
-        codepipeline.put_job_success_result(jobId=job["id"])
-    return 'Job Complete!'
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            listOfFileNames = myzip.namelist()
+
+            for fileName in listOfFileNames:
+                obj = myzip.open(fileName)
+                gloriashulman_bucket.upload_fileobj(obj, fileName, ExtraArgs={'ContentType': mimetypes.guess_type(fileName) [0]})
+                gloriashulman_bucket.Object(fileName).Acl().put(ACL='public-read')
+
+        topic.publish(Message="Portfolio Deployed!")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+        return 'Job Complete!'
+    except:
+        topic.publish(Message="Portfolio Deploy Failed!")
+        raise
+
 
